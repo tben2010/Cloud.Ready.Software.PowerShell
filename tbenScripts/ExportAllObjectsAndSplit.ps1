678153a29914aec418398eedae57c7e37f49371b
tben2010 <tben@me.com>
2016-12-08 09:20:35 +0100

    Revert "MyFirstScripts"
    This reverts commit 77d8f2899f2ec0a26f394ba806eeb3f3889d0235.

--- tbenScripts/ExportAllObjectsAndSplit.ps1
+++
@@ -1,7 +0,0 @@
-﻿$ServerInstance = 'DynamicsNAV100'
-$ObjectPath = 'c:\AllObjects.txt'
-$LogPath = 'c:\LogExportAllObjects'
-
-Export-NAVApplicationObject2 -ServerInstance $ServerInstance -Path $ObjectPath -LogPath $LogPath
-
-Split-NAVApplicationObjectFile -Source $ObjectPath -Destination 'c:\Users\tben\Dropbox\NAV2017\'
