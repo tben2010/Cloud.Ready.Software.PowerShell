c2462e27e7b3517da397fb8032e8f415d2dd814b
tben2010 <tben@me.com>
2016-12-13 16:20:13 +0100

    Unlock-NAVApplicationObjects: Bugfix for SQL Server instance support

---
+++ tbenScripts/InstallCuUpdate.ps1
@@ -0,0 +1,13 @@
+﻿#Automatischer Download wird ausgeführt
+$LaenderCode = 'W1'
+$DownloadPfad = 'C:\_Workingfolder'
+
+Get-NAVCumulativeUpdateFile -versions 2017 -CountryCode $LaenderCode -DownloadFolder $DownloadPfad
+
+#Aktuelle Build Version holen
+#Wie kann ich automatisch den -Path bzw.den Dateinamen ermittlen
+
+$CuBuild = Get-Content -Raw -Path C:\_Workingfolder\NAV.10.0.14199.W1.DVD.CU01.json | ConvertFrom-Json | Select Build, filename
+
+$CuBuild = $CuBuild.psobject.properties.value
+write-host -ForegroundColor Yellow $CuBuild
